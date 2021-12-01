0145148881fa1c2216a9b78437c15bd646498b65
MRCL <marcelo17922@gmail.com>
2021-11-30 20:50:00 -0600

    master

--- fisica/Ep.js
+++ fisica/Ep.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function epbasica() {
   let masa = parseFloat(prompt("Introduce la masa"));
   let altura = parseFloat(prompt("Introduce la altura"));
@@ -19,25 +18,3 @@ function epbasica() {
   `
   );
 }
-=======
-function epbasica() {
-  let masa = parseFloat(prompt("Introduce la masa"));
-  let altura = parseFloat(prompt("Introduce la altura"));
-  let r = masa * altura * 9.81;
-  swal(
-    `
-  ENERGIA POTENCIAL`,
-    `Formula:
-  Ep = m·g·h
-  -----
-  Recursos:
-  m = ${masa}kg
-  h = ${altura}m
-  g = 9.81m/s²
-  -----
-  Resultado:
-  Ep = ${r}J
-  `
-  );
-}
->>>>>>> fb7bc5d2d94794095ae387559eeed47e76fb05a0
